a2941a6e043154549992249ee065701f69c388ad
Jacques Testoni <jtestoni@lopes.com.br>
2021-05-31 11:24:17 -0300

    change connection string

--- src/config/PgConnection.ts
+++ src/config/PgConnection.ts
@@ -2,6 +2,7 @@ import { Pool } from 'pg';
 
 export default new Pool ({
     max: 20,
-    connectionString: 'postgres://postgres:postgres@localhost:5432/WSO2AM_DB',
+    //connectionString: 'postgres://postgres:postgres@localhost:5432/WSO2AM_DB',
+    connectionString: 'postgres://postgres:postgres@172.17.0.2:5432/WSO2AM_DB',
     idleTimeoutMillis: 1000
 });
